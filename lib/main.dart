9b0a095a9c218d4ed8a9d057ae8e4443b43c6498
David_DiBenedetto <david@njwebsolutions.net>
2024-02-18 18:14:27 +0000

    change title

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.orange,
         brightness: Brightness.dark,
       ),
-      home: MyHomePage(title: 'Heathwater Investment Research Tools'),
+      home: MyHomePage(title: 'Heathwater Investing'),
     );
   }
 }
